7cc9e85018d66e4cd49e413f8d0a56a9f8a58696
Gorbovskoy Grigoriy Vladimirovich <grishaxtail@gmail.com>
2023-06-17 23:14:25 +0300

    Initial Commit

---
+++ raspisansu/raspisansuApp.swift
@@ -0,0 +1,17 @@
+//
+//  raspisansuApp.swift
+//  raspisansu
+//
+//  Created by g.gorbovskoy on 17.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct raspisansuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
